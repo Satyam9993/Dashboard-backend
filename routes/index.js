61a83a35bc1a50f45d0b98a78042e004b81cd87e
Satyam Katiyar <satyamkatiyar9993@gmail.com>
2023-05-04 19:07:30 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const Data = require('../models/Data')
+
+router.get('/data', async (req, res) => {
+    Data.find({}).then(data => {
+        res.status(200).send({ data: data })
+    }).catch(err => {
+        res.status(402).send({ err: err })
+    });
+})
+
+
+router.put('/data1', async (req, res) => {
+    const {
+        end_year,
+        sector,
+        region,
+        pestle,
+        country,
+        topic,
+        source,
+        title
+    } = req.body;
+
+    const regextitle = new RegExp(title, 'i'); 
+    const regexcountry = new RegExp(country, 'i'); 
+    const regextopic = new RegExp(topic, 'i'); 
+    const regexsource = new RegExp(source, 'i'); 
+
+    await Data.find({ title: { $regex: regextitle }, country: { $regex: regexcountry }, topic: { $regex: regextopic }, source: { $regex: regexsource }, end_year: { $in: end_year }, sector: { $in: sector }, region: { $in: region }, pestle: { $in: pestle }  }).then(data => {
+        console.log(data.length);
+        res.status(200).send({ data: data })
+    }).catch(err => {
+        res.status(402).send({ err: err })
+    });
+})
+
+module.exports = router;
